69c32885c3080e3a8f84788b98b477586b983f10
obaedulislam <obaed.cse@gmail.com>
2022-10-24 17:55:05 +0600

    Server Created successfull

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 
 const port  = process.env.PORT || 5000;
 
+
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 
 app.get('/', (req, res) => {
@@ -16,6 +18,28 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories);
 });
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(id === '08'){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
 
 app.listen(port, () => {
     console.log("Dragon news server running on port:", port);
